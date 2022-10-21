a2f0b3db153b0e13ec2eac0b18728593bbe9aa1c
Felipe Felix <felippe.felix98@gmail.com>
2022-10-21 16:07:48 -0300

    chore: usar postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_10_21_023146) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "doses", force: :cascade do |t|
     t.string "tipo"
     t.date "data_vacinacao"
