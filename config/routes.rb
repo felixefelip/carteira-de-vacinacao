92ae7939d7835da8f7e48f62b71cf05d326c640c
Felipe Felix <felippe.felix98@gmail.com>
2023-02-19 20:50:18 -0300

    fix: erro nas rotas do devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root 'caderneta#index'
 
-  # devise_for :users
+  devise_for :users
 
-  devise_for :users, controllers: {
-    registrations: 'users/registrations'
-  }
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 
   resources :fabricante_vacinas
 
