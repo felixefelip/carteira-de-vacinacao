4e25a5f29b313bd24a0bec053765c9e3d579f4d3
Felipe Felix <felippe.felix98@gmail.com>
2023-02-22 22:28:28 -0300

    chore: remover pasta test

--- test/mailers/previews/rental_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/rental_mailer
-class RentalMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/rental_mailer/confirmation
-  def confirmation
-    RentalMailer.confirmation
-  end
-
-end
