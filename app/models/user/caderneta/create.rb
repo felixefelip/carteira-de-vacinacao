cfeae05250a8d5e8459e7825773d64c33c68610c
Felipe Felix <felippe.felix98@gmail.com>
2023-05-14 22:18:20 -0300

    fix: user services

--- app/models/user/caderneta/create.rb
+++ app/models/user/caderneta/create.rb
@@ -1,28 +1,26 @@
-class User
-  module Caderneta
-    class Create
-      attr_accessor :user
+module User::Caderneta
+  class Create
+    attr_accessor :user
 
-      def initialize(user)
-        @user = user
-      end
+    def initialize(user)
+      @user = user
+    end
 
-      def self.call!(user)
-        new(user).call!
-      end
+    def self.call!(user)
+      new(user).call!
+    end
 
-      def call!
-        criar_calendario_de_vacinacao!
-      end
+    def call!
+      criar_calendario_de_vacinacao!
+    end
 
-      def criar_calendario_de_vacinacao!
-        vacinas_do_calendario = Vacina.where.not(ordem_no_calendario: nil)
+    def criar_calendario_de_vacinacao!
+      vacinas_do_calendario = Vacina.where.not(ordem_no_calendario: nil)
 
-        recomendacao = Recomendacao.create!(user_id: user.id)
+      recomendacao = Recomendacao.create!(user_id: user.id)
 
-        vacinas_do_calendario.each do |vacina|
-          recomendacao.recomendacao_vacinas.create!(vacina: vacina)
-        end
+      vacinas_do_calendario.each do |vacina|
+        recomendacao.recomendacao_vacinas.create!(vacina: vacina)
       end
     end
   end
--- app/models/user/caderneta/update.rb
+++ app/models/user/caderneta/update.rb
@@ -1,25 +1,23 @@
-module User
-  module Caderneta
-    class Update
-      attr_accessor :user
+module User::Caderneta
+  class Update
+    attr_accessor :user
 
-      def initialize(user)
-        @user = user
-      end
+    def initialize(user)
+      @user = user
+    end
 
-      def self.call!(user)
-        new(user).call!
-      end
+    def self.call!(user)
+      new(user).call!
+    end
 
-      def call!
-        atualizar_calendario!
-      end
+    def call!
+      atualizar_calendario!
+    end
 
-      def atualizar_calendario!
-        user.recomendacao.recomendacao_vacinas.each do |recomendacao_vacina|
-          recomendacao_vacina.calcular_status_vacinal
-          recomendacao_vacina.save!
-        end
+    def atualizar_calendario!
+      user.recomendacao.recomendacao_vacinas.each do |recomendacao_vacina|
+        recomendacao_vacina.calcular_status_vacinal
+        recomendacao_vacina.save!
       end
     end
   end
