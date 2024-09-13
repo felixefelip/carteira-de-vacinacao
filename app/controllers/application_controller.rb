f6f0fa154a0d0e8c21981da0d12e80b884c84fc1
Felipe Felix <felipe.felix@maino.com.br>
2024-09-13 20:41:39 -0300

    fix: current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def current_user
+    User::Record.find(session['warden.user.user.key']['id'])
+  end
 end
