25751e7bcd4bb7346c0df5054d3a95799917c4cd
Felipe Felix <felipe.felix@maino.com.br>
2024-10-20 15:46:27 -0300

    chore: run rails app:update

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_07_06_030108) do
-
+ActiveRecord::Schema[8.0].define(version: 2024_10_20_184352) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "dose_do_calendarios", force: :cascade do |t|
     t.float "idade_recomendada"
     t.bigint "vacina_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["vacina_id"], name: "index_dose_do_calendarios_on_vacina_id"
   end
 
@@ -30,8 +29,8 @@ ActiveRecord::Schema.define(version: 2024_07_06_030108) do
     t.string "vacinador_codigo"
     t.string "local_codigo"
     t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.bigint "fabricante_vacina_id", null: false
     t.index ["fabricante_vacina_id"], name: "index_doses_on_fabricante_vacina_id"
     t.index ["user_id"], name: "index_doses_on_user_id"
@@ -41,17 +40,27 @@ ActiveRecord::Schema.define(version: 2024_07_06_030108) do
     t.string "descricao"
     t.bigint "user_id"
     t.bigint "vacina_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_fabricante_vacinas_on_user_id"
     t.index ["vacina_id"], name: "index_fabricante_vacinas_on_vacina_id"
+  end
+
+  create_table "fiscal_nota_fiscal_items", force: :cascade do |t|
+    t.decimal "icms_aliquota"
+    t.decimal "icms_valor"
+    t.decimal "icms_valor_base_de_calculo"
+    t.decimal "valor_unitario"
+    t.decimal "quantidade"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "recomendacao_vacinas", force: :cascade do |t|
     t.bigint "recomendacao_id"
     t.bigint "vacina_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "status_vacinal"
     t.index ["recomendacao_id"], name: "index_recomendacao_vacinas_on_recomendacao_id"
     t.index ["vacina_id"], name: "index_recomendacao_vacinas_on_vacina_id"
@@ -59,8 +68,8 @@ ActiveRecord::Schema.define(version: 2024_07_06_030108) do
 
   create_table "recomendacaos", force: :cascade do |t|
     t.bigint "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_recomendacaos_on_user_id"
   end
 
@@ -68,10 +77,10 @@ ActiveRecord::Schema.define(version: 2024_07_06_030108) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.date "data_nascimento"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
@@ -79,8 +88,8 @@ ActiveRecord::Schema.define(version: 2024_07_06_030108) do
 
   create_table "vacinas", force: :cascade do |t|
     t.string "descricao"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "dias_de_intervalo", default: 0, null: false
     t.integer "ordem_no_calendario"
   end
