81c821c090593a3fee9ce0479c02bde0e7e44645
Felipe Felix <felippe.felix98@gmail.com>
2025-04-05 23:20:55 -0300

    refactor: user phlex

---
+++ app/controllers/doses/new.rb
@@ -0,0 +1,29 @@
+# typed: true
+
+module Doses
+  class New < Phlex::HTML
+    extend T::Sig
+    # include Phlex::Rails::Helpers::FormWith
+
+    sig { params(dose: Dose::Record).void }
+    def initialize(dose:)
+      super()
+      @dose = dose
+    end
+
+    def view_template
+      byebug
+      div class: 'row mt-4' do
+        div class: 'col-10 col-lg-10 mx-auto' do
+          h1 { 'Nova Dose' }
+          render partial('form', dose:)
+        end
+      end
+    end
+
+    private
+
+    sig { returns(Dose::Record) }
+    attr_reader :dose
+  end
+end
