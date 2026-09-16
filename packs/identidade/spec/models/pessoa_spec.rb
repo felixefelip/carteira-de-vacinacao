92dc3e934176b3e34f1c5f129cf0db1cac55c16e
Felipe Felix <felippe.felix98@gmail.com>
2026-09-16 20:07:18 -0300

    extract avatar spec

--- packs/identidade/spec/models/pessoa_spec.rb
+++ packs/identidade/spec/models/pessoa_spec.rb
@@ -47,36 +47,4 @@ RSpec.describe Pessoa, type: :model do
       expect(pessoa.errors.full_messages).to include('E-mail já pertence a outra conta')
     end
   end
-
-  describe '#iniciais' do
-    it 'usa as duas primeiras palavras do nome' do
-      expect(FactoryBot.build(:pessoa, nome: 'ana clara souza').iniciais).to eq('AC')
-    end
-  end
-
-  describe 'avatar', :aggregate_failures do
-    it 'recusa arquivo que não é imagem' do
-      pessoa = FactoryBot.build(:pessoa)
-      pessoa.avatar.attach(io: StringIO.new('não sou imagem'), filename: 'texto.txt', content_type: 'text/plain')
-
-      expect(pessoa).to be_invalid
-      expect(pessoa.errors.full_messages).to include('Foto precisa ser PNG, JPEG ou WEBP')
-    end
-
-    it 'recusa imagem acima do tamanho máximo' do
-      pessoa = FactoryBot.build(:pessoa)
-      conteudo = StringIO.new('x' * (Pessoa::Avatar::TAMANHO_MAXIMO + 1))
-      pessoa.avatar.attach(io: conteudo, filename: 'grande.png', content_type: 'image/png')
-
-      expect(pessoa).to be_invalid
-      expect(pessoa.errors.full_messages).to include('Foto precisa ter no máximo 5 MB')
-    end
-
-    it 'aceita imagem dentro do limite' do
-      pessoa = FactoryBot.build(:pessoa)
-      pessoa.avatar.attach(io: StringIO.new('imagem'), filename: 'foto.png', content_type: 'image/png')
-
-      expect(pessoa).to be_valid
-    end
-  end
 end
